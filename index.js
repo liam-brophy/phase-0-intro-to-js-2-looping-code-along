62bc6fa9abe9a66126a3148000f006a7c16ec839
Liam Brophy <liambrophy.design@gmail.com>
2024-12-05 15:00:33 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+  const messages = [];
+  for (let i = 0; i < name.length; i++) {
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return messages;
+}
+
+
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(interger){
+
+  while (interger >= 0) {
+    console.log(interger);
+    interger --;
+  }
+}
+console.log(countDown("13"));
